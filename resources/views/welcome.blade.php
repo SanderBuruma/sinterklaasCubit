f234ea697b784cf7bc97a0126e662dd7a49584ca
SanderBuruma <sanderburuma@gmail.com>
2018-11-19 13:21:33 +0100

    divying things up into parials

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,24 +1,17 @@
 @extends('layouts.main')
 @section('content')
 <div class="flex-center position-ref full-height">
-	@if (Route::has('login'))
-		<div class="top-right links">
-			@auth
-				<a href="{{ url('/welcome') }}">Home</a>
-			@else
-				<a href="{{ route('login') }}">Login</a>
-
-				@if (Route::has('register'))
-					<a href="{{ route('register') }}">Register</a>
-				@endif
-			@endauth
-		</div>
-	@endif
-
 	<div class="content">
 		<div class="title m-b-md">
 				Sinterklaas
 		</div>
+		<h5>Verlanglijst</h5>
+		@if(\Auth::check())
+			<h6>Vertel Sinterklaas wie jij bent zodat hij weet wie jij bent wanneer hij langs komt.</h6>
+		@else
+
+		@endif
+		<h3></h3>
 	</div>
 </div>
 @endsection
