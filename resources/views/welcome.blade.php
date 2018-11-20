93b4bb1247cfacc347449f55fd0f47931ceddde1
SanderBuruma <sanderburuma@gmail.com>
2018-11-20 10:55:39 +0100

    tentative wish list made

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,12 +18,25 @@ a:focus {
 
 @section('content')
 <div class="row">
+	@if(\Auth::check())
+	<div class="col-md-8 offset-md-2" style="background-color: red; color: white;">
+
+		<table>
+			<tbody>
+				<?php $count = 0; ?>
+				@foreach( Auth::user()->items as $item )
+				<tr id="item{{ $item->id }}" class="content-justify-center">
+					<td>{{ ++$count }}</td>
+					<td>{{ $item->wish }}</td>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+
+	@else
 	<div class="mx-auto title m-b-md">
-		@if(\Auth::check())
-			
-		@else
-			<h2><strong><a href="{{ route('register') }}">Vertel</a></strong> Sinterklaas wie je bent voordat je je verlanglijstje maakt!</h2>
-		@endif
+		<h2><strong><a href="{{ route('register') }}">Vertel</a></strong> Sinterklaas wie je bent voordat je je verlanglijstje maakt!</h2>
+	@endif
 	</div>
 </div>
 @endsection
